d9bfc251f73709672960c11a005296e75a389128
VSartz <elsartz@gmail.com>
2022-05-23 13:42:43 -0400

    can edit title

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -2,13 +2,16 @@ async function editFormHandler(event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="post-title"]').value.trim();
+  const post_content = document.querySelector('input[name="post-content"]').value;
+ 
   const id = window.location.toString().split('/')[
     window.location.toString().split('/').length - 1
   ];
   const response = await fetch(`/api/posts/${id}`, {
     method: 'PUT',
     body: JSON.stringify({
-      title
+      title,
+      post_content
     }),
     headers: {
       'Content-Type': 'application/json'
@@ -22,4 +25,4 @@ async function editFormHandler(event) {
   }
 }
 
-document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+document.querySelector('#edit-post-form').addEventListener('submit', editFormHandler);
